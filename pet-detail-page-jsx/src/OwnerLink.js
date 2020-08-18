53aa63e337955163f87102fd4accd8d7891a6c2f
Lauren <elle.rachel.writes@gmail.com>
2020-08-18 10:05:05 -1000

    walkthrough complete

---
+++ pet-detail-page-jsx/src/OwnerLink.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const OwnerLink = props =>
+    <a href={props.href}>last name, first name</a>
+;
+
+export default OwnerLink;
---
+++ pet-detail-page-jsx/src/OwnersList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import OwnerLink from './OwnerLink'
+
+const OwnersList = props =>
+    <>
+        <h2>Owners</h2>
+        <ul>
+            {props.owners.map(owner =>
+                <li key={owner.id}>
+                    <OwnerLink
+                        href={owner.id}
+                        firstName={owner.firstName}
+                        lastName={owner.lastName}>
+                    </OwnerLink>
+                </li>
+            )}
+        </ul>
+    </>
+;
+
+OwnersList.defaultProps = {
+  owners: []
+};
+
+export default OwnersList;
